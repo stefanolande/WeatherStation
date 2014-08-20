c7ddef96c078c809458c4117da2430a9c73b2326
Stefano Lande <landeste@gmail.com>
2014-08-20 19:47:42 +0200

    added ethernet with serial logging
    Sends data to a server with GET request

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,6 +1,13 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+
 #include <Adafruit_BMP085.h>
 #include <Wire.h>
 #include <dht.h>
+
+
+#define UPDATE_INTERVAL 900000L //intervallo di aggiornamento
 
 
 #define DHT22_PIN 2
@@ -18,6 +25,24 @@
 dht DHT;
 Adafruit_BMP085 bmp;
 
+//variabili per collegarsi a internet
+byte mac[] = { 
+  0xDE, 0xAA, 0xBE, 0xEF, 0xAE, 0xDE  };
+
+IPAddress ip(192,168,50,100); //indirizzo IP disponibile sulla rete
+
+IPAddress myDns(192,168,1,1);
+
+char server[] = "is0eir.altervista.org"; //sito web
+
+EthernetClient client;
+
+unsigned long lastConnectionTime = 0; //tempo di ultima connessione al server
+boolean lastConnected = false;    
+const unsigned long postingInterval = UPDATE_INTERVAL;
+
+String strURL = "";
+
 
 void setup()
 {
@@ -28,10 +53,59 @@ void setup()
     while (1) {
     }
   }
+
+  delay(1000); //attendo un secondo
+
+  if(!Ethernet.begin(mac)){
+    Serial.println("DHPC error");
+    while(1);
+  }
+
+  //invio al pc il mio IP
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop()
 {
+  
+    if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+  
+    // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+  if (!client.connected() && lastConnected) {
+    Serial.println();
+    Serial.println("Disconnessione");
+    client.stop();
+  }
+  
+  
+    // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+    sendData(); //carico i dati sul server
+  }
+
+  lastConnected = client.connected();
+
+}
+
+
+
+//calcolo il punto di rugiada
+double puntoDiRugiada(double temperatura, double umidita)
+{
+  double a = 17.271;
+  double b = 237.7;
+  double temp = (a * temperatura) / (b + temperatura) + log(umidita/100);
+  double Td = (b * temp) / (a - temp);
+  return Td;
+}
+
+void sendData(){
   double qnh;
   double umidita;
   double temp;
@@ -56,7 +130,56 @@ void loop()
     Serial.println(puntoDiRugiada(temp, umidita), 1);
     Serial.print("QNH: ");
     Serial.println(qnh, 1);
+    
+    Serial.println(server);
     Serial.println();
+
+    if (client.connect(server, 80))
+    {
+      Serial.println("Connesso");
+
+      char charBuf[7];
+
+      //creo l'url utilizzando una stringa
+      strURL ="GET /meteo/receive.php?temp=";
+
+      dtostrf(temp, 4, 1, charBuf);
+      strURL+= charBuf; 
+
+      strURL+= "&hum=";
+      dtostrf(umidita, 4, 1, charBuf);
+      strURL+= charBuf;
+
+      strURL+= "&dewp=";
+      dtostrf(puntoDiRugiada(temp, umidita), 4, 1, charBuf);
+      strURL+= charBuf;
+
+      strURL+="&qnh=" ;
+      dtostrf(qnh, 4, 1, charBuf);
+      strURL+= charBuf; 
+
+      strURL+=" HTTP/1.1";
+      
+      //strURL = "GET /search?q=arduino HTTP/1.1";
+
+      //invio la richiesta al server
+      client.println(strURL);
+      client.println("Host: is0eir.altervista.org");
+      client.println("Connection: close");
+      client.println();
+
+      lastConnectionTime = millis();
+
+      delay(1000);
+      Serial.println(strURL);
+    }
+    else
+    {
+      Serial.println("Errore Connessione");
+      Serial.println("Disconnessione");
+      client.stop();
+    }
+
 
     break;
   case DHTLIB_ERROR_CHECKSUM: 
@@ -68,18 +191,13 @@ void loop()
   default: 
     Serial.print("Unknown error,\t"); 
     break;
-  }
-
-
-  delay(5000);
+  } 
 }
 
-//calcolo il punto di rugiada
-double puntoDiRugiada(double temperatura, double umidita)
-{
-  double a = 17.271;
-  double b = 237.7;
-  double temp = (a * temperatura) / (b + temperatura) + log(umidita/100);
-  double Td = (b * temp) / (a - temp);
-  return Td;
-}
+
+
+
+
+
+
+
